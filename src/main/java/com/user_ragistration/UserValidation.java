6a812605ad90203642a6bd017d5d69445c2b9651
Birajit <birajit95@gmail.com>
2020-11-25 08:01:37 +0530

    Modify: Password pattern is modified to ensure Atleast One Special char

--- src/main/java/com/user_ragistration/UserValidation.java
+++ src/main/java/com/user_ragistration/UserValidation.java
@@ -9,7 +9,7 @@ public class UserValidation {
 	static final String NAME_PATTERN = "^[A-Z][a-z]{2,}$";
 	static final String EMAIL_PATTERN = "^[a-zA-Z]{1}[a-zA-Z0-9]+([-\\.\\_\\+]?[0-9a-zA-Z]+)*\\@[a-zA-Z]+([\\.][a-z]{2,4})?([\\.][a-z]{2,4})$";
 	static final String MOBILE_NUMBER_PATTERN = "^(\\+91|91)[ ]{1}[6-9]{1}[0-9]{9}$";
-	static final String PASSWORD_PATTERN = "(?=.*[A-Z])(?=.*[0-9])([a-zA-Z0-9]|[^a-zA-Z0-9]){8,}";
+	static final String PASSWORD_PATTERN = "(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]{1})([a-zA-Z0-9]|[^a-zA-Z0-9]){8,}";
 	
 	public String getPattern(String inputTitle) {
 		if(inputTitle.equals("First Name") || inputTitle.equals("Last Name")) {
